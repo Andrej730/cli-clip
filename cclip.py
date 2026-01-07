199ad128a4500f33d6418b4b6f1188e0fc0bc42f
Andrej730 <azhilenkov@gmail.com>
2026-01-07 12:52:20 +0500

    cpaste - fix bug with doubled lines on Windows

--- cclip.py
+++ cclip.py
@@ -1,6 +1,15 @@
+import os
 import sys
 
 import pyperclip
+
+# Necessary on Windows, otherwise it will replace each printed `\n` with `\r\n`
+# when `.paste()` might already have `\r\n` line endings, leading to doubled lines.
+if os.name == "nt":
+    from io import TextIOWrapper
+
+    assert isinstance(sys.stdout, TextIOWrapper)
+    sys.stdout.reconfigure(newline="\n")
 
 
 def copy() -> None:
